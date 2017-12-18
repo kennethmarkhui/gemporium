8857dcf7b1f48cbe7a9dc406ff9ec15a7355f0f6
kennethmarkhui <kennethmarkhui@gmail.com>
2017-12-18 17:48:31 +0800

    Modified Details Modal

--- index.php
+++ index.php
@@ -9,23 +9,39 @@
     <script src="js/bootstrap.min.js"></script>
   </head>
   <body>
+
     <!-- Navigation Bar -->
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
-        <a href="index.php" class="navbar-brand">Gemporium</a>
-        <ul class="nav navbar-nav">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jewelry<span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Necklace</a></li>
-              <li><a href="#">Bracelet</a></li>
-              <li><a href="#">Earring</a></li>
-              <li><a href="#">Ring</a></li>
-            </ul>
-          </li>
-        </ul>
+
+        <!-- Mobile Display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Gemporium</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Necklaces</a></li>
+            <li><a href="#">Bracelets</a></li>
+            <li><a href="#">Earrings</a></li>
+            <li><a href="#">Rings</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Others <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Watches</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
+
     <!-- Header -->
     <div id="headerWrapper"></div>
 
@@ -110,9 +126,9 @@
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-labe="Close">
+            <!-- <button type="button" class="close" data-dismiss="modal" aria-labe="Close">
               <span aria-hidden="true">&times;</span>
-            </button>
+            </button> -->
             <h4 class="modal-title text-center">Tiffany's Necklace</h4>
           </div>
           <div class="modal-body">
@@ -129,25 +145,37 @@
                   <hr>
                   <p>Price: ₱50</p>
                   <p>Brand: Tiffany</p>
+                  <hr />
                   <form action="add_cart.php" method="post">
                     <div class="form-group">
                       <div class="col-xs-3">
                         <label for="quantity">Quantity:</label>
                         <input type="number" class="form-control" id="quantity" name="quantity" min="0">
                       </div>
+                      <!-- <p>Available: 3</p> -->
+                    </div>
+                    <div class="form-group">
                       <div class="col-xs-3">
-                        <p>Available: 3</p>
+                        <label for="size">Size:</label>
+                        <select name="size" id="size" class="form-control">
+                          <option value=""></option>
+                          <option value="16">16</option>
+                          <option value="17">17</option>
+                          <option value="18">18</option>
+                        </select>
                       </div>
                     </div>
-                  </form>
+                  </form><br><br><br><br><hr>
+                  <button type="submit" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-shopping-cart"></span>Add to Cart</button>
+                  <button type="button" class="btn btn-default btn-block" data-dismiss="modal">Close</button>
                 </div>
               </div>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-shopping-cart"></span>Add to Cart</button>
-          </div>
+          <!-- <div class="modal-footer">
+
+
+          </div> -->
         </div>
       </div>
     </div>
