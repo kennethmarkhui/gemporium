c03e3251ceb9a2f21c3103c64a83052e729a38d5
kennethmarkhui <kennethmarkhui@gmail.com>
2017-12-18 14:41:57 +0800

    Navigation bar with menu(static) Header Background Image

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gemporium</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <a href="index.php" class="navbar-brand">Gemporium</a>
+        <ul class="nav navbar-nav">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jewelry<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">Necklace</a></li>
+              <li><a href="#">Bracelet</a></li>
+              <li><a href="#">Earring</a></li>
+              <li><a href="#">Ring</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Header -->
+    <div id="headerWrapper"></div>
+  </body>
+</html>
