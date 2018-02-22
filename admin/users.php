a4f6b650cb2577b4e3e407235ece1a2f9482b8da
kennethmarkhui <kennethmarkhui@gmail.com>
2018-02-22 13:39:39 +0800

    Edit admin page users to update database

--- admin/users.php
+++ admin/users.php
@@ -8,25 +8,39 @@
   }
   include 'includes/head.php';
   include 'includes/navigation.php';
+  //Delete user
   if (isset($_GET['delete'])) {
     $deleteID = sanitize($_GET['delete']);
     $db->query("DELETE FROM adminusers WHERE id = '$deleteID'");
     $_SESSION['success_flash'] = "User has been deleted!";
     header("Location: users.php");
   }
-  if (isset($_GET['add'])) {
+  //if add/edit button is clicked
+  if (isset($_GET['add']) || isset($_GET['edit'])) {
     $name = ((isset($_POST['name']))?sanitize($_POST['name']):'');
     $email = ((isset($_POST['email']))?sanitize($_POST['email']):'');
     $password = ((isset($_POST['password']))?sanitize($_POST['password']):'');
     $confirm = ((isset($_POST['confirm']))?sanitize($_POST['confirm']):'');
     $permissions = ((isset($_POST['permissions']))?sanitize($_POST['permissions']):'');
+    // repopulate the fields when editing a user
+    if (isset($_GET['edit'])) {
+      $editUserID = (int)$_GET['edit'];
+      $usersEdit = $db->query("SELECT * FROM adminusers WHERE id = '$editUserID'");
+      $userEdit = mysqli_fetch_assoc($usersEdit);
+
+      $name = ((isset($_POST['name']))?sanitize($_POST['name']):$userEdit['full_name']);
+      $email = ((isset($_POST['email']))?sanitize($_POST['email']):$userEdit['email']);
+      $permissions = ((isset($_POST['permissions']))?sanitize($_POST['permissions']):$userEdit['permissions']);
+    }
     // Validation
     $errors = array();
     if ($_POST) {
-      $emailQuery = $db->query("SELECT * FROM adminusers WHERE email = '$email'");
-      $emailCount = mysqli_num_rows($emailQuery);
-      if ($emailCount != 0) {
-        $errors[] = 'That email already exist in our database.';
+      if (isset($_GET['add'])) {
+        $emailQuery = $db->query("SELECT * FROM adminusers WHERE email = '$email'");
+        $emailCount = mysqli_num_rows($emailQuery);
+        if ($emailCount != 0) {
+          $errors[] = 'That email already exist in our database.';
+        }
       }
       $required = array('name', 'email', 'password', 'confirm', 'permissions');
       foreach ($required as $adduserfield) {
@@ -49,14 +63,19 @@
       }else{
         //Add user to database
         $hashed = password_hash($password,PASSWORD_DEFAULT);
-        $db->query("INSERT INTO adminusers (full_name,email,password,permissions) VALUES ('$name','$email','$hashed','$permissions')");
-        $_SESSION['success_flash'] = 'User has been added!';
+        if (isset($_GET['add'])) {
+          $db->query("INSERT INTO adminusers (full_name,email,password,permissions) VALUES ('$name','$email','$hashed','$permissions')");
+          $_SESSION['success_flash'] = 'User has been added!';
+        } elseif (isset($_GET['edit'])) {
+          $db->query("UPDATE adminusers SET full_name = '$name', email = '$email', password = '$hashed', permissions = '$permissions' WHERE id ='$editUserID'");
+          $_SESSION['success_flash'] = 'User has been updated!';
+        }
         header("Location: users.php");
       }
     }
     ?>
-    <h2 class="text-center">Add a New User</h2><hr>
-    <form action="users.php?add=1" method="post">
+    <h2 class="text-center"><?php echo ((isset($_GET['edit']))?'Edit':'Add a New');?> User</h2><hr>
+    <form action="users.php?<?php echo ((isset($_GET['edit']))?'edit='.$editUserID:'add=1');?>" method="post">
       <div class="form-group col-md-6">
         <label for="name">Full Name:</label>
         <input type="text" name="name" id="name" class="form-control" value="<?php echo $name;?>">
@@ -83,7 +102,7 @@
       </div>
       <div class="form-group col-md-6 text-right" id="add-user-submit-button">
         <a href="users.php" class="btn btn-default">Cancel</a>
-        <input type="submit" value="Add User" class="btn btn-primary">
+        <input type="submit" value="<?php echo ((isset($_GET['edit']))?'Edit':'Add');?> User" class="btn btn-primary">
       </div>
     </form>
     <?php
@@ -107,7 +126,8 @@
       <tr>
         <td>
           <?php if($user['id'] != $userData['id']):?>
-            <a href="users.php?delete=<?php echo $user['id'];?>" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-remove-sign"></span></a>
+            <a href="users.php?edit=<?php echo $user['id'];?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+            <a href="users.php?delete=<?php echo $user['id'];?>" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-remove"></span></a>
           <?php endif;?>
         </td>
         <td><?php echo $user['full_name'];?></td>
